b002244095329342224ae380bb2c6f7ac61cd571
SaurabhBelsare13 <saurabhbelsare4@gmail.com>
2024-11-27 18:29:20 +0530

    rename application.py to app.py & adding runtime.txt file

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-application = Flask(__name__)
-app = application
+app = Flask(__name__)
+
 
 
 # importing ridge regressor and standard scalar pickle
